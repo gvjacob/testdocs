b1df75945fdee1e3d7e29a055430c340fb970053
Gino Jacob <gvjacob@outlook.com>
2019-03-13 14:59:14 -0400

    getMarkdownString

--- extension.js
+++ extension.js
@@ -1,32 +1,64 @@
-const { commands, languages, window, Hover } = require('vscode');
+const {
+  commands,
+  languages,
+  workspace,
+  extensions,
+  window,
+  Hover,
+  MarkdownString,
+} = require('vscode');
 const { ExtensionContext } = require('vscode');
 
 /**
  * @param {ExtensionContext} context
  */
 function activate(context) {
-  console.log('Congratulations, your extension "testdocs" is now active!');
+  console.log(workspace.name, workspace.rootPath);
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = commands.registerCommand('extension.helloWorld', function() {
-    // The code you place here will be executed every time your command is executed
+  let disposable = commands.registerCommand('extension.testdocs', function() {
+    // Register the hover provider
     languages.registerHoverProvider('javascript', {
-      provideHover(document, position, token) {
-        console.log(document, position, token);
-        return new Hover([
-          document.fileName,
-          position.line.toString(),
-          position.character.toString(),
-        ]);
+      provideHover({ fileName }, { line, character }) {
+        const markdown = getMarkdown(fileName, line, character);
+        return new Hover(markdown);
       },
     });
-    // Display a message box to the user
   });
 
   context.subscriptions.push(disposable);
 }
+
+/**
+ * Get the markdown test documentation from hovered
+ * word in the given document.
+ *
+ * @param {string} fileName path to file with hover
+ * @param {int} line line number
+ * @param {int} character character number
+ * @returns {string} markdown string
+ */
+function getMarkdown(fileName, line, character) {
+  // const hoveredModule = getHoveredModule(fileName, line, character);
+  // const moduleDirectory = getModuleDirectory(hoveredModule);
+  // const testFileName = getTestFileName(moduleDirectory);
+  // const { moduleName, testSuites } = getTestFileStructure(testFileName);
+  return new MarkdownString(
+    '### Module ### \n' + 'should return a string\n' + '\nshould only take in a string \n',
+  );
+}
+
+/**
+ * Get the module of the hovered item in file.
+ *
+ * @param {string} fileName
+ * @param {int} line
+ * @param {int} character
+ * @returns {string}
+ */
+function getHoveredModule(fileName, line, character) {}
 
 // this method is called when your extension is deactivated
 function deactivate() {}
